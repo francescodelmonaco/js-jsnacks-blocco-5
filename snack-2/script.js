de8ee6a7878819aa5e5ede2159b0a119416183ed
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-27 12:31:17 +0100

    add: snack-2 code

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,6 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const peopleNames = people.map( (element) => element.name);
+console.log(peopleNames);
